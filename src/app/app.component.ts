3dd8ec87da819357719ec57e528aeeed655902c6
lijiang <lijiang@gogotrans.com>
2017-09-30 11:44:44 +0700

    初步了解angular animation动画

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
 import {Component} from '@angular/core';
 import {OverlayContainer} from '@angular/material';
 
+// 动画
+import {trigger, state, transition, style, animate} from '@angular/animations';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    // 定义一个触发器
+    trigger('square',
+      [
+        state('green',
+          style({
+            'background-color': 'green',
+            'height': '100px',
+            'transform': 'translateX(0)'
+          })),
+
+        state('red', style({
+          'background-color': 'red',
+          'height': '50px',
+          'transform': 'translateX(100%)'
+        })),
+        transition('green => red', animate('.2s 1s')),
+        transition('red => green', animate(500)),
+
+      ])
+  ]
 })
 export class AppComponent {
+  squareState: string;
 
   // 为设置弹出组件方便， 全局定义OverlayContainer
   constructor(private oc: OverlayContainer) {
@@ -14,4 +39,33 @@ export class AppComponent {
   }
 
   title = 'app';
+
+
+  onClick() {
+    this.squareState = this.squareState === 'red' ? 'green' : 'red';
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// 动画模块
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import {MdSidenavModule} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +22,7 @@ import {ProjectModule} from './project/project.module';
     AppRoutingModule,
     LoginModule,
     ProjectModule,
+    BrowserAnimationsModule,  // 动画模块,最好最后导入
   ],
   providers: [],
   bootstrap: [AppComponent]
