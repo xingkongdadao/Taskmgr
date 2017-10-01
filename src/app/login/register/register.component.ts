6d88c9d952ba7e6c9ec10130305cafc8038628c2
lijiang <lijiang@gogotrans.com>
2017-10-01 21:49:09 +0700

    注册页面添加表单功能，完善图片选择功能。但是默认随机选择功能还没有实现。 $符号不行。

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -11,10 +11,11 @@ export class RegisterComponent implements OnInit {
   form: FormGroup;
   items: string[];
 
-  constructor( private fb: FormBuilder) {
+  constructor(private fb: FormBuilder) {
   }
 
   ngOnInit() {
+
     const img = '${this.avatarName}:svg-${Math.floor(Math.random() * 16).toFixed(0)}';
 
     const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
