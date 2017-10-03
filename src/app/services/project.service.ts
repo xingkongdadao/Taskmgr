f9bb8448ea6e78f20f82769dc4bdb2fb827217a8
lijiang <lijiang@gogotrans.com>
2017-10-03 09:04:06 +0700

    添加模型接口，建立项目服务，添加get，post, patch, delete方法。

---
+++ src/app/services/project.service.ts
@@ -0,0 +1,61 @@
+import {Inject, Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import {Project} from '../domain/project.model';
+import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+export class ProjectService {
+
+  private readonly domain = 'projects';
+  public headers = new Headers({
+    'Content-Type': 'application/json',
+  });
+
+  constructor(private http: Http, @Inject('BASE_CONFIG') private config) {
+  }
+
+  // post
+  add(project: Project): Observable<Project> {
+    project.id = null;
+    const uri = this.config.uri + this.domain;
+    return this.http
+      .post(uri, JSON.stringify(project), {headers: this.headers})
+      .map(res => res.json());
+  }
+
+  // patch
+  update(project: Project): Observable<Project> {
+    const uri = this.config.uri + this.domain + project.id;
+    const toUpdate = {
+      name: project.name,
+      desc: project.desc,
+      coverImg: project.coverImg
+    };
+    return this.http
+      .patch(uri, JSON.stringify(toUpdate), {headers: this.headers})
+      .map(res => res.json());
+  }
+
+  // delete
+  delete(project: Project): Observable<Project> {
+    const deltask$ = Observable.from(project.taskLists ? project.taskLists : [])
+      .mergeMap(listId => this.http
+        .delete(this.config.uri + '/taskLists/' + listId))
+      .count();
+    const uri = this.config.uri + '/' + this.domain + '/' + project.id;
+    return deltask$.switchMap(p => this.http
+      .delete(uri)
+      .map(_ => project));
+  }
+
+  // GET /projects
+  get(userId: string): Observable<Project[]> {
+    const uri = this.config.uri + `/` + this.domain;
+    return this.http
+      .get(uri, {params: {'members_like': userId}, headers: this.headers})
+      .map(res => res.json());
+  }
+
+
+
+}
