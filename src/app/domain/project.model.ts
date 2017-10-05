2f7207356a04f801744c5bcdb35428a9d910c917
lijiang <lijiang@gogotrans.com>
2017-10-05 13:26:19 +0700

    添加project： enabled?: boolean;

--- src/app/domain/project.model.ts
+++ src/app/domain/project.model.ts
@@ -2,7 +2,8 @@ export interface Project {
   id?: string;
   name: string;
   desc?: string;
-  coverImg: string;
-  taskLists?: string[];  // 列表id
-  members?: string[];  // 成员 id
+  coverImg?: string;
+  enabled?: boolean;
+  taskLists?: string[]; // 存储 TaskList ID
+  members?: string[]; // 存储成员 key 为 ID， value 为角色
 }
