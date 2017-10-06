f7f736422ee09058f9633cc7746dc60e2ec3b2c9
lijiang <lijiang@gogotrans.com>
2017-10-06 10:42:43 +0700

    添加 import {AuthEffects} from './auth.effects';

--- src/app/effects/effects.module.ts
+++ src/app/effects/effects.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {EffectsModule} from '@ngrx/effects';
 import {QuoteEffects} from './quote.effects';
+import {AuthEffects} from './auth.effects';
 
 @NgModule({
   imports: [
     EffectsModule.run(QuoteEffects),
+    EffectsModule.run(AuthEffects),
     CommonModule
   ],
   declarations: []
